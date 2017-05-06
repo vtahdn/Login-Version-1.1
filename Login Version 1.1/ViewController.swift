0698460d8ca49c56cbd66ecc7c47d83ff7247852
Viet Anh Doan <vtahdn@icloud.com>
2017-05-06 18:16:03 +0700

    login 1.1

--- Login Version 1.1/ViewController.swift
+++ Login Version 1.1/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textFieldUserName: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    var userList = ["apple":"ios","techmaster":"14","viet":"123"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func buttonActionLogin(_ sender: UIButton) {
+        if let password = userList[textFieldUserName.text!] {
+            if password == textFieldPassword.text {
+                print("Successful.")
+            }
+            else {
+                print("Invalid password.")
+            }
+        }
+        else {
+            print("Invalid username.")
+        }
+    }
+    @IBAction func buttonActionUsers(_ sender: UIButton) {
+        print(userList)
+    }
+    @IBAction func buttonActionRegister(_ sender: UIButton) {
+        userList[textFieldUserName.text!] = textFieldPassword.text
+    }
+    
+    
 }
 
